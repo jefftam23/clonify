c0200178f24ee8eef5c786a89afd35460614cdb5
Jeffrey Tam <jefftam23@gmail.com>
2017-02-16 17:47:19 -0500

    Add image attachments to albums

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,15 +2,22 @@
 #
 # Table name: albums
 #
-#  id         :integer          not null, primary key
-#  name       :string           not null
-#  artist_id  :integer          not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                 :integer          not null, primary key
+#  name               :string           not null
+#  artist_id          :integer          not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  image_file_name    :string
+#  image_content_type :string
+#  image_file_size    :integer
+#  image_updated_at   :datetime
 #
 
 class Album < ActiveRecord::Base
   validates :name, :artist, presence: true
+
+  has_attached_file :image, default_url: "missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   belongs_to :artist
   has_many :songs
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216160356) do
+ActiveRecord::Schema.define(version: 20170216224549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "albums", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.integer  "artist_id",  null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",               null: false
+    t.integer  "artist_id",          null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["artist_id"], name: "index_albums_on_artist_id", using: :btree
   end
 
