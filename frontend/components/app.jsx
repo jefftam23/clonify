b74a99e3deb9a064a25c1a8e73c1807af0e1725a
Jeffrey Tam <jefftam23@gmail.com>
2017-02-15 21:48:40 -0500

    Add main navbar component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import MainNav from './main_nav';
 
 const App = ({ children }) => (
   <div>
     <h1>Clonify</h1>
+    <MainNav />
     { children }
   </div>
 );
---
+++ frontend/components/main_nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainNav = () => {
+  return (
+    <div>
+      <p>I'm the main navbar thing</p>
+    </div>
+  );
+};
+
+export default MainNav;
