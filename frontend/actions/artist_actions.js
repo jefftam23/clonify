def82640b2cef2c2d03f4fcb6f7250f6e84d645e
Jeffrey Tam <jefftam23@gmail.com>
2017-02-16 13:52:32 -0500

    Add thunk action creator for fetching all artists

--- frontend/actions/artist_actions.js
+++ frontend/actions/artist_actions.js
@@ -2,6 +2,13 @@ import * as ArtistApiUtil from '../util/artist_api_util';
 
 export const RECEIVE_ALL_ARTISTS = "RECEIVE_ALL_ARTISTS";
 
+export const fetchAllArtists = () => {
+  return (dispatch) => {
+    return ArtistApiUtil.fetchAllArtists()
+      .then(artists => dispatch(receiveAllArtists(artists)));
+  };
+};
+
 export const receiveAllArtists = (artists) => {
   return {
     type: RECEIVE_ALL_ARTISTS,
