2a7bb716c6491860d2015917cf6a91f55eecda36
Jeffrey Tam <jefftam23@gmail.com>
2017-02-17 15:49:53 -0500

    Position and style logout and current user on navbar

--- frontend/components/main_nav.jsx
+++ frontend/components/main_nav.jsx
@@ -4,47 +4,56 @@ import { logout } from '../actions/session_actions';
 import { connect } from 'react-redux';
 
 const MainNav = ({ logout }) => {
-  debugger;
   return (
     <header className="main-nav-wrapper">
       <nav className="main-nav">
 
-        <div className="nav-logo">
-          <Link to="/browse">
-            <img src={ window.images.logo } />
-          </Link>
+        <div className="top-nav">
+
+          <div className="nav-logo">
+            <Link to="/browse">
+              <img src={ window.images.logo } />
+            </Link>
+          </div>
+
+          <ul className="main-nav-items">
+
+            <li className="browse-nav">
+              <Link to="/browse">
+                <img src={ window.images.browseIcon } />
+                <p className="nav-item-text">Browse</p>
+              </Link>
+            </li>
+
+            <li className="your-music-nav">
+              <Link to="/browse">
+                <img src={ window.images.yourMusicIcon } />
+                <p className="nav-item-text">Your Music</p>
+              </Link>
+            </li>
+
+            <li className="follow-nav">
+              <Link to="/browse">
+                <img src={ window.images.followIcon } />
+                <p className="nav-item-text">Follow</p>
+              </Link>
+            </li>
+
+          </ul>
+
         </div>
 
-        <ul className="main-nav-items">
+        <div className="bottom-nav">
 
-          <li className="browse-nav">
-            <Link to="/browse">
-              <img src={ window.images.browseIcon } />
-              <p className="nav-item-text">Browse</p>
-            </Link>
-          </li>
+          <div className="current-user">
+            <p>{ window.currentUser.username }</p>
+          </div>
 
-          <li className="your-music-nav">
-            <Link to="/browse">
-              <img src={ window.images.yourMusicIcon } />
-              <p className="nav-item-text">Your Music</p>
-            </Link>
-          </li>
+          <button className="logout-button"
+            onClick={ logout }>LOG OUT
+          </button>
 
-          <li className="follow-nav">
-            <Link to="/browse">
-              <img src={ window.images.followIcon } />
-              <p className="nav-item-text">Follow</p>
-            </Link>
-          </li>
-
-          <li>
-            <button className="logout-button"
-              onClick={ logout }>LOGOUT
-            </button>
-          </li>
-
-        </ul>
+        </div>
       </nav>
     </header>
   );
