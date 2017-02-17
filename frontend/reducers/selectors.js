ca6022d59366b63a079ca007197968cf2e12983a
Jeffrey Tam <jefftam23@gmail.com>
2017-02-17 09:19:57 -0500

    Add artist albums selector

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -3,3 +3,7 @@ import { values } from 'lodash';
 export const selectAllArtists = ({ allArtists }) => {
   return values(allArtists);
 };
+
+export const selectArtistAlbums = ({ artistDetails }) => {
+  return values(artistDetails.albums);
+};
