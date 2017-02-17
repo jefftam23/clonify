7ab24ab9b0658e5c86295a9420fe5e6a870484c1
Jeffrey Tam <jefftam23@gmail.com>
2017-02-17 09:18:43 -0500

    Destructure artist props object

--- frontend/components/browse/artist_index_item.jsx
+++ frontend/components/browse/artist_index_item.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const ArtistIndexItem = ({ artist }) => {
+const ArtistIndexItem = ({ artist: { id, name, imageUrl } }) => {
+
   return (
     <li>
-      <Link to="/"><img src={ artist.image_url } /></Link>
-      <p>{ artist.name }</p>
+      <Link to={ `/artists/${id}` }><img src={ imageUrl } /></Link>
+      <p>{ name }</p>
     </li>
   );
 };
