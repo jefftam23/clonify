3d7c2c22cce478d9cf21dbf063f3b533e5e88590
Jeffrey Tam <jefftam23@gmail.com>
2017-02-19 18:11:21 -0500

    Add AudioPlayer component to App

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MainNav from './main_nav';
+import AudioPlayerContainer from './audio_player/audio_player_container';
 
 const App = ({ children }) => (
   <div className="app-wrapper">
     <MainNav />
     { children }
+    <AudioPlayerContainer />
   </div>
 );
 
