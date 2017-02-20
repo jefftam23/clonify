f9d7765a0ac5e8275d12b7f3c2165770dc5968a6
Jeffrey Tam <jefftam23@gmail.com>
2017-02-20 13:20:37 -0500

    Add ability to toggle play/pause songs

--- frontend/components/audio_player/audio_player.jsx
+++ frontend/components/audio_player/audio_player.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 import NowPlaying from './now_playing';
 import SongControls from './song_controls';
 
-const AudioPlayer = ({ nowPlaying }) => {
-  if (Boolean(nowPlaying)) {
-    return (
-      <div className="audio-player">
-        <NowPlaying nowPlaying={ nowPlaying }/>
-        <SongControls />
-      </div>
-    );
-  } else {
-    return (
-      <div className="no-current-song"></div>
-    );
+class AudioPlayer extends React.Component {
+  render() {
+    const { nowPlaying } = this.props;
+
+    if (Boolean(nowPlaying)) {
+      return (
+        <div className="audio-player">
+          <NowPlaying nowPlaying={ nowPlaying } />
+          <SongControls nowPlaying={ nowPlaying } />
+        </div>
+      );
+    } else {
+      return (
+        <div className="no-current-song"></div>
+      );
+    }
+
   }
-};
+}
 
 export default AudioPlayer;
--- frontend/components/audio_player/song_controls.jsx
+++ frontend/components/audio_player/song_controls.jsx
@@ -1,11 +1,55 @@
 import React from 'react';
 
-const SongControls = () => {
-  return (
-    <div className="song-controls">
-      <p>Song Controls PLACEHOLDER</p>
-    </div>
-  );
-};
+class SongControls extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { playing: "" };
+
+    this.togglePlay = this.togglePlay.bind(this);
+  }
+
+  componentDidMount() {
+    this.audioEl = document.getElementById('audio');
+    this.audioEl.play();
+    this.setState({ playing: !this.audioEl.paused });
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (this.props.nowPlaying.id !== newProps.nowPlaying.id) {
+      this.setState({ playing: true }); // autoPlay starts the new song
+    }
+  }
+
+  togglePlay() {
+    if (this.audioEl.paused) {
+      this.audioEl.play();
+      this.setState({ playing: true });
+    } else {
+      this.audioEl.pause();
+      this.setState({ playing: false });
+    }
+  }
+
+  render() {
+    const { nowPlaying } = this.props;
+
+    return (
+      <div className="song-controls">
+        <audio
+          id="audio"
+          controls="controls"
+          src={ nowPlaying.audioUrl }
+          type="audio/mpeg"
+          autoPlay>Your browser does not support the audio element.
+        </audio>
+
+        <button
+          onClick={ this.togglePlay }>
+          { this.state.playing ? "Pause" : "Play" }
+        </button>
+      </div>
+    );
+  }
+}
 
 export default SongControls;
