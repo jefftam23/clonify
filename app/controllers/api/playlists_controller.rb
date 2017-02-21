98c6b3e6ce58f1ed8c3067d4df1082aadfcb14a4
Jeffrey Tam <jefftam23@gmail.com>
2017-02-21 18:40:51 -0500

    Add create action

--- app/controllers/api/playlists_controller.rb
+++ app/controllers/api/playlists_controller.rb
@@ -3,4 +3,19 @@ class Api::PlaylistsController < ApplicationController
     @playlists = User.find(params[:user_id]).playlists
     render :index
   end
+
+  def create
+    @playlist = Playlist.new(playlist_params)
+
+    if @playlist.save
+      render :show
+    else
+    end
+  end
+
+  private
+
+  def playlist_params
+    params.require(:playlist).permit(:name, :owner_id)
+  end
 end
