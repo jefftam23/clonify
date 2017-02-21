e91795518d97400778a7a4be5c0408a47d938498
Jeffrey Tam <jefftam23@gmail.com>
2017-02-21 13:57:18 -0500

    Create playlist model

---
+++ app/models/playlist.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: playlists
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  owner_id   :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Playlist < ActiveRecord::Base
+  validates :name, :owner, presence: true
+
+  belongs_to :owner,
+    class_name: 'User',
+    primary_key: :id,
+    foreign_key: :owner_id
+
+  # has_many :playlist_listings
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170219162150) do
+ActiveRecord::Schema.define(version: 20170221184457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,14 @@ ActiveRecord::Schema.define(version: 20170219162150) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "playlists", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.integer  "owner_id",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_playlists_on_owner_id", using: :btree
   end
 
   create_table "songs", force: :cascade do |t|
