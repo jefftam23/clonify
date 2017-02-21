2ca2066471296b5a6b5b5c023ecf9f5dba12f0ab
Jeffrey Tam <jefftam23@gmail.com>
2017-02-21 17:19:46 -0500

    Fix what playlists get retrieved from the db

--- app/controllers/api/playlists_controller.rb
+++ app/controllers/api/playlists_controller.rb
@@ -1,6 +1,6 @@
 class Api::PlaylistsController < ApplicationController
   def index
-    @playlists = current_user.playlists
+    @playlists = User.find(params[:user_id]).playlists
     render :index
   end
 end
