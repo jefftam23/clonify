b35a4f3c04e09eaf1c520cedccc9280beca2167f
Jeffrey Tam <jefftam23@gmail.com>
2017-02-21 19:09:23 -0500

    Add thunk and regular action creators for creating and receiving a single playlist, respectively

--- frontend/actions/playlist_actions.js
+++ frontend/actions/playlist_actions.js
@@ -1,6 +1,7 @@
 import * as PlaylistApiUtil from '../util/playlist_api_util';
 
 export const RECEIVE_USER_PLAYLISTS = "RECEIVE_USER_PLAYLISTS";
+export const RECEIVE_SINGLE_USER_PLAYLIST = "RECEIVE_SINGLE_USER_PLAYLIST";
 
 export const fetchUserPlaylists = (userId) => {
   return (dispatch) => {
@@ -9,9 +10,23 @@ export const fetchUserPlaylists = (userId) => {
   };
 };
 
+export const createUserPlaylist = (userId, playlist) => {
+  return (dispatch) => {
+    return PlaylistApiUtil.createUserPlaylist(userId, playlist)
+      .then(respPlaylist => dispatch(receiveSingleUserPlaylist(respPlaylist)));
+  };
+};
+
 export const receiveUserPlaylists = (playlists) => {
   return {
     type: RECEIVE_USER_PLAYLISTS,
     playlists
+  };
+};
+
+export const receiveSingleUserPlaylist = (playlist) => {
+  return {
+    type: RECEIVE_SINGLE_USER_PLAYLIST,
+    playlist
   };
 };
