199c7e202c72a1c6e8362cae6e1ae156132dd171
Jeffrey Tam <jefftam23@gmail.com>
2017-02-21 19:10:05 -0500

    Update reducer to receive single playlist

--- frontend/reducers/your_music_playlists_reducer.js
+++ frontend/reducers/your_music_playlists_reducer.js
@@ -1,4 +1,7 @@
-import { RECEIVE_USER_PLAYLISTS } from '../actions/playlist_actions';
+import {
+  RECEIVE_USER_PLAYLISTS,
+  RECEIVE_SINGLE_USER_PLAYLIST } from '../actions/playlist_actions';
+import merge from 'lodash/merge';
 
 const YourMusicPlaylistsReducer = (oldState = {}, action) => {
   Object.freeze(oldState);
@@ -6,6 +9,10 @@ const YourMusicPlaylistsReducer = (oldState = {}, action) => {
   switch (action.type) {
     case RECEIVE_USER_PLAYLISTS:
       return action.playlists;
+
+    case RECEIVE_SINGLE_USER_PLAYLIST:
+      return merge({}, oldState, action.playlist);
+
     default:
       return oldState;
   }
