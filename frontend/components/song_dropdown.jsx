e7c6d12c43a107b0c60357ace7b3c2f9f00949b3
Jeffrey Tam <jefftam23@gmail.com>
2017-02-23 16:06:27 -0500

    Style dropdown and show on click rather than hover

--- frontend/components/song_dropdown.jsx
+++ frontend/components/song_dropdown.jsx
@@ -1,60 +1,87 @@
 import React from 'react';
 import { values } from 'lodash';
 
-const SongDropdown = ({ songId,
-                        listingId,
-                        playlists,
-                        fetchSongDetails,
-                        deletePlaylistListing,
-                        createPlaylistListing,
-                        playlistView }) => {
+class SongDropdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { display: false };
 
-  let playlistArr = [];
-  if (Boolean(playlists)){
-    playlistArr = values(playlists);
+    this.toggleDisplay = this.toggleDisplay.bind(this);
   }
 
-  // get the yourMusicPlaylists slice of state
-  // If it hasn't yet been populated, fetch them
-  const addToPlaylistButtons = playlistArr.map((playlist, idx) => {
-    const listing = { playlist_id: playlist.id, song_id: songId };
+  toggleDisplay() {
+    const nextState = !this.state.display;
+    this.setState({ display: nextState });
+  }
 
-    return (
-      <li key={ idx }>
+  render() {
+    const { songId,
+            listingId,
+            playlists,
+            fetchSongDetails,
+            deletePlaylistListing,
+            createPlaylistListing,
+            playlistView } = this.props;
+
+    let playlistArr = [];
+    if (Boolean(playlists)){
+      playlistArr = values(playlists);
+    }
+
+    // get the yourMusicPlaylists slice of state
+    // If it hasn't yet been populated, fetch them
+    const addToPlaylistButtons = playlistArr.map((playlist, idx) => {
+      const listing = { playlist_id: playlist.id, song_id: songId };
+
+      return (
+        <li key={ idx }>
+          <button
+            onClick={ () => createPlaylistListing(listing) }>&nbsp;
+              <span>Add to { playlist.name }</span>
+          </button>
+        </li>
+      );
+    });
+
+    const deleteButton = (
+      <li>
         <button
-          onClick={ () => createPlaylistListing(listing) }>Add to { playlist.name }
+          onClick={ () => deletePlaylistListing(listingId) }>&nbsp;
+            <span>Delete</span>
         </button>
       </li>
     );
-  });
 
-  const deleteButton = (
-    <li>
-      <button
-        onClick={ () => deletePlaylistListing(listingId) }>Delete
-      </button>
-    </li>
-  );
+    let menu;
+    if (this.state.display) {
+      menu = (
+        <ul>
+          <li>
+            <button
+              onClick={ () => fetchSongDetails(songId) }>&nbsp;
+                <span>Play</span>
+            </button>
+          </li>
 
-  return (
-    <ul className="song-dropdown">
-      <li>
+          { addToPlaylistButtons }
 
-      <ul>
-        <li>
-          <button
-            onClick={ () => fetchSongDetails(songId) }>Play
-          </button>
-        </li>
+          { playlistView ? deleteButton : "" }
+        </ul>
+      );
+    } else {
+      menu = (
+        <ul></ul>
+      );
+    }
 
-        { addToPlaylistButtons }
+    return (
+      <div className="song-dropdown">
+        <button onClick={ this.toggleDisplay }>...</button>
+        { menu }
+      </div>
+    );
+  }
 
-        { playlistView ? deleteButton : "" }
-      </ul>
-
-      </li>
-    </ul>
-  );
-};
+}
 
 export default SongDropdown;
