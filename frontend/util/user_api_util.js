bd2d474cf80881959b1a5c7109c0615639be0e47
Jeffrey Tam <jefftam23@gmail.com>
2017-03-07 17:20:33 -0500

    Create AJAX util functions for creating and deleting user follows

--- frontend/util/user_api_util.js
+++ frontend/util/user_api_util.js
@@ -4,3 +4,18 @@ export const fetchAllUsers = () => {
     url: '/api/users'
   });
 };
+
+export const createUserFollow = (userFollow) => {
+  return $.ajax({
+    method: 'POST',
+    url: '/api/user_follows',
+    data: { userFollow }
+  });
+};
+
+export const deleteUserFollow = (id) => {
+  return $.ajax({
+    method: 'DELETE',
+    url: `/api/user_follows/${id}`
+  });
+};
