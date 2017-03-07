c14d787bead26f706c3585254d08ca4ff596e5fb
Jeffrey Tam <jefftam23@gmail.com>
2017-03-07 17:01:08 -0500

    Remove store and action from window

--- frontend/clonify.jsx
+++ frontend/clonify.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-import { fetchAllUsers } from './actions/user_actions';
-window.fetchAllUsers = fetchAllUsers;
-
 document.addEventListener('DOMContentLoaded', () => {
   let store;
   if (window.currentUser) {
@@ -14,8 +11,6 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     store = configureStore();
   }
-
-  window.store = store;
 
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store } />, root);
