4c2e05f5a91c3a9b3a945ad397db3b5c4cd1ea9c
Jeffrey Tam <jefftam23@gmail.com>
2017-03-09 17:40:15 -0500

    Remove minification warning on production

---
+++ webpack.config.prod.js
@@ -0,0 +1,37 @@
+var webpack = require("webpack");
+
+module.exports = {
+  entry: './frontend/clonify.jsx',
+  output: {
+    path: 'app/assets/javascripts',
+    filename: 'bundle.js',
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
