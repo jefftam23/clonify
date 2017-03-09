6edad125df9a13012ba38f502f9139228fb613b0
Jeffrey Tam <jefftam23@gmail.com>
2017-03-09 08:31:23 -0500

    Add receive user details action

--- frontend/actions/user_actions.js
+++ frontend/actions/user_actions.js
@@ -2,11 +2,19 @@ import * as UserApiUtil from '../util/user_api_util';
 
 export const RECEIVE_ALL_USERS = "RECEIVE_ALL_USERS";
 export const RECEIVE_SINGLE_USER = "RECEIVE_SINGLE_USER";
+export const RECEIVE_USER_DETAILS = "RECEIVE_USER_DETAILS";
 
 export const fetchAllUsers = () => {
   return (dispatch) => {
     return UserApiUtil.fetchAllUsers()
       .then(users => dispatch(receiveAllUsers(users)));
+  };
+};
+
+export const fetchUserDetails = (id) => {
+  return (dispatch) => {
+    return UserApiUtil.fetchUserDetails(id)
+      .then(userDetails => dispatch(receiveUserDetails(userDetails)));
   };
 };
 
@@ -35,5 +43,12 @@ export const receiveSingleUser = (user) => {
   return {
     type: RECEIVE_SINGLE_USER,
     user
+  };
+};
+
+export const receiveUserDetails = (userDetails) => {
+  return {
+    type: RECEIVE_USER_DETAILS,
+    userDetails
   };
 };
